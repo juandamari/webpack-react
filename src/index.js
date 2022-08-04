83c77b3e260b762366f213178e031678daa611c4
juan david M <juan_da-mari@hotmail.com>
2022-08-03 22:16:50 -0500

    error de imrpimir "hello"

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import React from "react";
+/* import React from "react";
 import { ReactDOM } from "react-dom";
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+ */
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/App';
+
+/* ReactDom.render(<App/>, document.getElementById('app')); */
+ReactDOM.createRoot(document.getElementById('App')).render(<App />)
